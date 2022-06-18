4eb60b97fb509e7343fab0cf446598def80e6c76
kaeriv93 <vsan93@yahoo.com>
2022-06-18 15:21:54 -0500

    added to URL will test

--- base/apiroute/urls.py
+++ base/apiroute/urls.py
@@ -9,6 +9,8 @@ from rest_framework_simplejwt.views import (
 urlpatterns =[
     path('', views.getRoutes),
     path('posts/', views.getPosts),
+    path('users/', views.getUsers),
+    path('profiles/', views.getProfiles),
     path('api/token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
