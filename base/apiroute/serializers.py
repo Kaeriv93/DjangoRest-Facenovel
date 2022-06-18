ef35dcc868cb1e0e078293d28e06fe9f36aeec3a
kaeriv93 <vsan93@yahoo.com>
2022-06-18 15:15:17 -0500

    attempting to add user sterializer

--- base/apiroute/serializers.py
+++ base/apiroute/serializers.py
@@ -1,6 +1,7 @@
 from dataclasses import fields
 from rest_framework.serializers import ModelSerializer
 from base.models import Post, Profile
+from django.contrib.auth.models import User
 
 class PostSerializer(ModelSerializer):
     class Meta:
@@ -10,4 +11,9 @@ class PostSerializer(ModelSerializer):
 class Profile(ModelSerializer):
     class Meta:
         model = Profile
+        fields = '__all__'
+        
+class User(ModelSerializer):
+    class Meta:
+        model = User
         fields = '__all__'
